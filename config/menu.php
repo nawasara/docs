ff60727ca7bd63cf5c53c42c04e674ae0ca30034
Pringgo J. Saputro <odyinggo@gmail.com>
2026-04-24 13:50:31 +0700

    fix(menu): replace heroicon references with lucide icons

--- config/menu.php
+++ config/menu.php
@@ -5,27 +5,27 @@ $prefix = 'nawasara-docs';
 return [
     [
         'label' => 'Components',
-        'icon' => 'heroicon-o-cube',
+        'icon' => 'lucide-boxes',
         'url' => '',
         'permission' => 'nawasara-core.component.view',
         'submenu' => [
             [
                 'label' => 'Table',
-                'icon' => 'heroicon-o-puzzle-piece',
+                'icon' => 'lucide-table-2',
                 'url' => url($prefix.'/components/table'),
                 'permission' => 'nawasara-core.component.view',
                 'navigate' => true,
             ],
             [
                 'label' => 'Base Komponen',
-                'icon' => 'heroicon-o-puzzle-piece',
+                'icon' => 'lucide-puzzle',
                 'url' => url($prefix.'/components/base'),
                 'permission' => 'nawasara-core.component.view',
                 'navigate' => true,
             ],
             [
                 'label' => 'Form Komponen',
-                'icon' => 'heroicon-o-puzzle-piece',
+                'icon' => 'lucide-form-input',
                 'url' => url($prefix.'/components/form'),
                 'permission' => 'nawasara-core.component.view',
                 'navigate' => true,
